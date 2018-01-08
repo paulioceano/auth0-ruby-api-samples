0a32c4be66b48c20476d20cdf46ca2607614e901
Alexis Luque <aluque@indexlabs.com.uy>
2018-01-08 16:12:05 -0300

    Update endpoints

--- 01-Authorization-RS256/lib/jwt/json_web_token.rb
+++ 01-Authorization-RS256/lib/jwt/json_web_token.rb
@@ -7,7 +7,7 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithm: 'RS256',
-               iss: Sinatra::Application.settings.auth0_domain,
+               iss: "https://#{Sinatra::Application.settings.auth0_domain}/",
                verify_iss: true,
                aud: Sinatra::Application.settings.auth0_api_audience,
                verify_aud: true) do |header|
@@ -18,7 +18,7 @@ class JsonWebToken
   def self.jwks_hash
     ## NOTE: This call should be cached on production environments to avoid requesting the kid everytime
     ## and thus prevent having DDoS attacks
-    jwks_raw = Net::HTTP.get URI("#{Sinatra::Application.settings.auth0_domain}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://#{Sinatra::Application.settings.auth0_domain}/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
